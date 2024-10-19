d6b5bde9b7b57c317a3b8bf0436c314935129689
YawAsanteBoadu <yawasante95@yahoo.com>
2024-10-19 11:58:46 +0000

    sorting highscore updated

--- end.js
+++ end.js
@@ -4,14 +4,33 @@ const finalScore = document.getElementById('finalScore');
 
 // retrieve score from localStorage and display on end page// 
 const mostRecentScores = localStorage.getItem('mostRecentScores');
+
 finalScore.innerText = `Score: ${mostRecentScores}`;
 
-username.addEventListener('keyup', ()=>{
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+console.log(highScores)
+
+const MAX_HIGH_SCORE = 5;
+
+username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 })
 // save high score//
 saveHighScore = ((e) => {
     console.log('save button works');
     e.preventDefault();
+
+    const scores = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    }
+
+    highScores.push(scores);  // pushing the scores object into the array highscores//
+    
+    highScores.sort((a,b) => a.score - b.score) // find out the difference in arranging the par for sorting 
+    highScores.splice(MAX_HIGH_SCORE);
+
+    console.log(highScores)
+
 })
 
