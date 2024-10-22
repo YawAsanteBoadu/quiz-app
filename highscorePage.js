a24ea6629a08031663e84fe29f4b022d3f0760f7
YawAsanteBoadu <yawasante95@yahoo.com>
2024-10-22 08:54:35 +0000

    High Score list updated

--- highscorePage.js
+++ highscorePage.js
@@ -3,6 +3,8 @@ const highScoresList = document.getElementById('highScoresList');
 // get the highscore out of local storage//
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-highScores.map( scores=> {
-    console.log(`${scores.name}-${scores.score}`)
+highScoresList.innerHTML = highScores
+.map( scores=> {
+    return`<li class="dsiplay-highScore">${scores.name}:  ${scores.score} </li>`
 })
+.join("")
